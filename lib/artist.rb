a1d4fc616d64ca65c15ec0eba664bb684a85e5f4
Zoncharne Lee <zoncharne@gmail.com>
2019-03-26 08:31:53 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+  end
+  
+  def add_song(song_name)
+    song.artist = self
+  end
+  
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+  
+end
---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+  attr_accessor :title, :author
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+  
+  def author_name
+    author.name if author
+  end
+  
+end
