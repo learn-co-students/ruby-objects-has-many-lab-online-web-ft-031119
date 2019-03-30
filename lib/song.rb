3932228330a2eea33638715690fc0e40c474dc9a
ddlou <darren.lou@gmail.com>
2019-03-30 11:38:16 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -17,6 +17,7 @@ class Song
     end
 
     def artist_name
+        # nice shorthand expression for returning the artist name if there is an artist or nil if not
         artist.name if artist
     end
 end
