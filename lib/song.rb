accf9a03ea65c94ee8e6bcb14f57e38def877f01
ddlou <darren.lou@gmail.com>
2019-03-30 11:36:57 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
 class Song
-    attr_accessor :title, :artist
+    attr_accessor :name, :artist
     
     @@all = []
 
-    def initialize(title)
-        @title = title
+    def initialize(name)
+        @name = name
         @@all << self
     end
 
     def artist_name
-        self.artist.title
+        self.artist.name
     end
 
     def self.all
@@ -17,6 +17,6 @@ class Song
     end
 
     def artist_name
-        artist.title if artist
+        artist.name if artist
     end
 end
